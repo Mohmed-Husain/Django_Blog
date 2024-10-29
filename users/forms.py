08bdf68c01293f975a608684b55cfb512abda9d7
Mohmed Husain <arkonafoob@gmail.com>
2024-10-29 14:55:57 +0530

    Signup/register complited with crispy

---
+++ users/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+        labels = {
+            'username': 'User Name',
+            'email': 'Email Address'
+        }
+        help_texts = {
+            'username': 'Enter a unique username.',
+            'email': 'Enter a valid email address for account confirmation.'
+        }
+        widgets = {
+            'password1': forms.PasswordInput(attrs={'placeholder': 'Enter your password'}),
+            'password2': forms.PasswordInput(attrs={'placeholder': 'Confirm your password'})
+        }
