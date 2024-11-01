aa87ee4f88200132dffef118e4b3b09d812fb618
Mohmed Husain <arkonafoob@gmail.com>
2024-11-01 21:31:50 +0530

    User Profile Upadate Function Added!

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-
+from .models import Profile 
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -9,15 +9,21 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
-        labels = {
-            'username': 'User Name',
-            'email': 'Email Address'
-        }
-        help_texts = {
-            'username': 'Enter a unique username.',
-            'email': 'Enter a valid email address for account confirmation.'
-        }
-        widgets = {
-            'password1': forms.PasswordInput(attrs={'placeholder': 'Enter your password'}),
-            'password2': forms.PasswordInput(attrs={'placeholder': 'Confirm your password'})
-        }
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("This email is already in use.")
+        return email
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email'] 
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
