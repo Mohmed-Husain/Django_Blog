0592029bc78e10f3f5b6760e8fae7a973dea026a
Mohmed Husain <arkonafoob@gmail.com>
2024-11-03 10:15:27 +0530

    Detail View added

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView,DetailView, CreateView
 from .models import Post
 
 def home(request):
@@ -6,6 +7,16 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model= Post 
+    template_name='blog/home.html'
+    context_object_name='posts'
+    ordering=['-date']
+
+class PostDetailView(DetailView):
+    model= Post 
+    
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
